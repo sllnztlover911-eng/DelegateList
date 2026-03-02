88461938d322a75a3923e3f868ba3cdad81a9a3d
adedilnaz <adedilnaz@gmail.com>
2026-03-02 16:26:09 +0500

    Initial Commit

---
+++ DelegateList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegateList
+//
+//  Created by Асылхан Дильназ on 02.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
